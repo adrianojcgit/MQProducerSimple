f6d70807ba78c986365cd425af5a11acc9434cf7
adrianojcgit <48298141+adrianojcgit@users.noreply.github.com>
2024-01-30 16:55:11 -0300

    Versão inicial

---
+++ Producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "qwebapp",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+        int count = 0;
+        while (true)
+        {
+            string message = $"{count++} Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: "qwebapp",
+                                 basicProperties: null,
+                                 body: body);
+            Thread.Sleep(200);
+        }
+    }
+}
